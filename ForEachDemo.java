99d0924e70615c770a20f9fece4aad3575279739
Bigyanhub <bigyanbasnet849@gmail.com>
2026-01-21 22:34:08 +0545

    Add ForEachDemo class to demonstrate enhanced for-loop usage in Java

---
+++ ForEachDemo.java
@@ -0,0 +1,11 @@
+package Excercise;
+public class ForEachDemo {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for(int num : numbers){
+            System.out.println("Numbers: " + num);
+        }
+    }
+    
+}
