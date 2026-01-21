22b18d7fb1e7befa25d539f0d1ef2d3c3e418dbe
Bigyanhub <bigyanbasnet849@gmail.com>
2026-01-21 22:34:54 +0545

    Add MultiArrayDemo class to demonstrate multi-dimensional array usage in Java

---
+++ MultiArrayDemo.java
@@ -0,0 +1,20 @@
+package Excercise;
+public class MultiArrayDemo {
+
+    public static void main(String[] args) {
+
+        int[][] table = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9, 10, 11 }
+        };
+        for (int r = 0; r < table.length; r++) {
+            for (int c = 0; c < table[r].length; c++) {
+                System.out.println(table[r][c] + " " + "[" + r + "]" + "[" + c + "]");
+            }
+
+            System.out.println();
+        }
+    };
+
+}
